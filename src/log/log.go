c2c698a3fbeaebb6b31de3326a55629f5f6c4fd2
Agang <ligang1109@outlook.com>
2023-11-02 10:56:39 +0800

    update

--- src/log/log.go
+++ src/log/log.go
@@ -1,13 +1,24 @@
 package log
 
 import (
+	"fmt"
 	"io"
 	"os"
 
 	"github.com/goinbox/golog"
+	"github.com/goinbox/gomisc"
 )
 
 var Logger golog.Logger
+
+func Init() {
+	w := &consoleWriter{
+		Writer: os.Stderr,
+	}
+
+	Logger = golog.NewSimpleLogger(w, new(flatFormater)).
+		SetLogLevel(golog.LevelDebug).EnableColor()
+}
 
 type consoleWriter struct {
 	io.Writer
@@ -21,11 +32,21 @@ func (c consoleWriter) Free() {
 	return
 }
 
-func Init() {
-	w := &consoleWriter{
-		Writer: os.Stderr,
+var filterLogFieldKeyMap = map[string]bool{
+	"level": true,
+	"t":     true,
+}
+
+type flatFormater struct {
+}
+
+func (f *flatFormater) Format(fields ...*golog.Field) []byte {
+	var msg []byte
+	for _, field := range fields {
+		if !filterLogFieldKeyMap[field.Key] {
+			msg = gomisc.AppendBytes(msg, []byte(fmt.Sprintf("%v", field.Value)), []byte("\t"))
+		}
 	}
 
-	Logger = golog.NewSimpleLogger(w, golog.NewSimpleFormater()).
-		SetLogLevel(golog.LevelDebug).EnableColor()
+	return msg
 }
--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/goinbox/golog"
 
 	"glox/log"
 )
@@ -17,16 +20,41 @@ func main() {
 	}
 
 	if nargs == 2 {
-		runFile(os.Args[1])
+		err := runFile(os.Args[1])
+		if err != nil {
+			log.Logger.Error("runFile error", golog.ErrorField(err))
+		}
 	} else {
 		runPrompt()
 	}
 }
 
-func runFile(path string) {
+func runFile(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("os.Open error: %w", err)
+	}
+	defer func() {
+		_ = f.Close()
+	}()
 
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		run(scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return fmt.Errorf("scanner.Scan error: %w", err)
+	}
+
+	return nil
 }
 
 func runPrompt() {
 
+}
+
+func run(line string) {
+	fmt.Println(line)
 }
