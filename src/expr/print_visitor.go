bd431ccf2f2318fe48bad306b9b76d649b339a27
Agang <ligang1109@outlook.com>
2026-01-01 22:14:55 +0800

    update

--- src/expr/print_visitor.go
+++ src/expr/print_visitor.go
@@ -2,48 +2,142 @@ package expr
 
 import (
 	"fmt"
+	"strings"
 )
 
+type printVisitorNode struct {
+	row  int
+	col  int
+	text string
+
+	parent *printVisitorNode
+}
+
+func (n *printVisitorNode) nodeId() string {
+	return fmt.Sprintf("r%dc%d", n.row, n.col)
+}
+
+func (n *printVisitorNode) drawNode() string {
+	return fmt.Sprintf("%s[%s]", n.nodeId(), n.text)
+}
+
+func (n *printVisitorNode) drawConnect() string {
+	return fmt.Sprintf("%s --> %s", n.parent.nodeId(), n.nodeId())
+}
+
 type PrintVisitor struct {
-	curRow    int
-	rowColMap map[int]int
+	lineList []string
+
+	curNode      *printVisitorNode
+	colAssignMap map[int]int
 }
 
 func NewPrintVisitor() *PrintVisitor {
 	return &PrintVisitor{
-		rowColMap: map[int]int{},
+		colAssignMap: map[int]int{},
 	}
 }
 
-func (v *PrintVisitor) assignNodeCol(row int) int {
-	col, ok := v.rowColMap[row]
+func (v *PrintVisitor) assignCol(row int) int {
+	col, ok := v.colAssignMap[row]
 	if !ok {
 		col = 0
 	} else {
 		col++
 	}
 
-	v.rowColMap[row] = col
+	v.colAssignMap[row] = col
 
 	return col
 }
 
-func (v *PrintVisitor) visitExpr(exp Expr) {
+func (v *PrintVisitor) newNode(text string, parent *printVisitorNode) *printVisitorNode {
+	row := 0
+	if parent != nil {
+		row = parent.row + 1
+	}
+
+	return &printVisitorNode{
+		row:  row,
+		col:  v.assignCol(row),
+		text: text,
+
+		parent: parent,
+	}
+}
+
+func (v *PrintVisitor) drawNode(node *printVisitorNode) {
+	v.lineList = append(v.lineList, node.drawNode())
+	if node.parent != nil {
+		v.lineList = append(v.lineList, node.drawConnect())
+	}
+}
+
+func (v *PrintVisitor) drawChild(child *printVisitorNode, exp Expr) {
+	v.curNode = child
+	exp.Accept(v)
+	v.curNode = child.parent
+}
+
+func (v *PrintVisitor) Graph() string {
+	graph := "flowchart TD\n"
+	graph += strings.Join(v.lineList, "\n")
+	graph += "\n"
+
+	return graph
 }
 
 func (v *PrintVisitor) VisitBinary(binary *Binary) {
-	name := binary.Name()
-	graph := fmt.Sprintf("%s --> %s")
+	node := v.curNode
+	if node == nil {
+		node = v.newNode(binary.Name(), nil)
+	}
+	v.drawNode(node)
+
+	child := v.newNode(binary.Left.Name(), node)
+	v.drawChild(child, binary.Left)
+
+	child = v.newNode(fmt.Sprintf("Operator %s", binary.Operator.Lexeme), node)
+	v.drawNode(child)
+
+	child = v.newNode(binary.Right.Name(), node)
+	v.drawChild(child, binary.Right)
 }
 
 func (v *PrintVisitor) VisitGrouping(grouping *Grouping) {
+	text := grouping.Expression.Name()
+	child := v.curNode
+	if child == nil {
+		child = v.newNode(text, nil)
+	} else {
+		child.text = text
+	}
 
+	v.drawChild(child, grouping.Expression)
 }
 
 func (v *PrintVisitor) VisitLiteral(literal *Literal) {
+	text := fmt.Sprintf("%s %v", literal.Name(), literal.Value)
+	node := v.curNode
+	if node == nil {
+		node = v.newNode(text, nil)
+	} else {
+		node.text = text
+	}
 
+	v.drawNode(node)
 }
 
 func (v *PrintVisitor) VisitUnary(unary *Unary) {
+	node := v.curNode
+	if node == nil {
+		node = v.newNode(unary.Name(), nil)
+	}
+	v.drawNode(node)
 
+	child := v.newNode(fmt.Sprintf("Operator %s", unary.Operator.Lexeme), node)
+	v.drawNode(child)
+
+	child = v.newNode(unary.Right.Name(), node)
+	v.drawChild(child, unary.Right)
 }
---
+++ src/expr/print_visitor_test.go
@@ -0,0 +1,41 @@
+package expr
+
+import (
+	"glox/token"
+	"testing"
+)
+
+func TestPrintVisitor(t *testing.T) {
+	// (3+4)*5
+	binary := &Binary{
+		Left: &Grouping{
+			Expression: &Binary{
+				Left: &Literal{
+					Value: 3,
+				},
+				Right: &Literal{
+					Value: 4,
+				},
+				Operator: &token.Token{
+					Lexeme: "+",
+				},
+			},
+		},
+		Right: &Unary{
+			Right: &Literal{
+				Value: 5,
+			},
+			Operator: &token.Token{
+				Lexeme: "-",
+			},
+		},
+		Operator: &token.Token{
+			Lexeme: "*",
+		},
+	}
+
+	visitor := NewPrintVisitor()
+	binary.Accept(visitor)
+
+	t.Log(visitor.Graph())
+}
